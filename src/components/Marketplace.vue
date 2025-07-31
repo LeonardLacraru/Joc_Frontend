fff2974bdb762cfab6debe5b688a105a0431cddd
Leonard <leonard.lacraru@gmail.com>
2025-07-31 23:12:35 +0300

    more

--- src/components/Marketplace.vue
+++ src/components/Marketplace.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { authFetch } from "../utils/authFetch.js";
 import { ref, computed, onMounted } from "vue";
+import "../assets/inventory.css";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const inventory = ref([]);
@@ -8,6 +9,7 @@ const loading = ref(true);
 const SLOT_COUNT = 33;
 const activeItemId = ref(null);
 const marketplace = ref([]);
+const listed_items = ref([]);
 const prices = ref({});
 const profile = ref({});
 const statLabels = {
@@ -25,10 +27,7 @@ const statLabels = {
 
 const gridInventory = computed(() => {
   // Only include unequipped items
-  const unequipped = inventory.value.filter(
-    (item) => item && item.is_equipped === false && item.is_listed === false
-
-  );
+  const unequipped = inventory.value
   // Fill up to SLOT_COUNT with nulls
   const slots = Array(SLOT_COUNT).fill(null);
   for (let i = 0; i < unequipped.length && i < SLOT_COUNT; i++) {
@@ -53,6 +52,7 @@ function handleImageError(event) {
 onMounted(async () => {
   await fetchInventory();
   await fetchMarketplace();
+  await GetListedItems();
 });
 
 async function fetchInventory() {
@@ -64,7 +64,7 @@ async function fetchInventory() {
       inventory.value = data.inventory_items || 0;
       loading.value = false;
       return null;
-      
+
     } else {
       const errData = await response.json();
       alert(errData.detail || JSON.stringify(errData));
@@ -84,6 +84,7 @@ async function fetchMarketplace() {
       const data = await response.json();
       marketplace.value = data || [];
       loading.value = false;
+      console.log("Marketplace data fetched:", data);
       return null;
     } else {
       const errData = await response.json();
@@ -114,7 +115,7 @@ async function confirmSell(itemId) {
 
     if (response.ok) {
       await fetchInventory();
-      await fetchMarketplace();
+      await GetListedItems();
       prices.value[itemId] = 0;
       activeItemId.value = null;
     } else {
@@ -152,61 +153,143 @@ async function BuyItem(itemId) {
   }
 }
 
+async function GetListedItems() {
+  try {
+    const response = await authFetch(`${API_BASE_URL}/marketplace/get_listed_items/`);
+    if (response.ok) {
+      const data = await response.json();
+      listed_items.value = data || [];
+      console.log("Listed items fetched:", data);
+      return null;
+    } else {
+      const errData = await response.json();
+      alert(errData.detail || JSON.stringify(errData));
+    }
+  } catch (err) {
+    alert(err.message);
+    error.value = err.message;
+    loading.value = false;
+    return err.message;
+  }
+}
+
+async function CancelSell(itemId) {
+  try {
+    const response = await authFetch(`${API_BASE_URL}/marketplace/${itemId}/cancel/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      await fetchInventory();
+      await GetListedItems();
+      activeItemId.value = null;
+    } else {
+      const errData = await response.json();
+      alert(errData.detail || JSON.stringify(errData));
+    }
+  } catch (err) {
+    alert(err.message);
+    error.value = err.message;
+    loading.value = false;
+    return err.message;
+  }
+}
 
 </script>
 
 <template>
   <div class="main">
-    <div class="inventory-grid">
-      <div v-for="item in gridInventory" class="inventory-item">
-        <template v-if="item">
-          <div class="tooltip-container">
-            <img :src="generateImageName(item.item.name, item.item.rarity)" :alt="item.name" class="item-icon"
-              @error="handleImageError" />
-            <div class="custom-tooltip">
-              <div class="tt-font-name" :class="`rarity-${item.item.rarity}`">
-                {{ item.item.name }}
-              </div>
-              <div class="tt-font">
-                Required Level: {{ item.item.required_level }}
-              </div>
-              <div class="tt-stats" v-if="item.item.stats && item.item.stats.length">
-                Stats:
-                <div v-for="(stat, sidx) in item.item.stats" :key="sidx" class="tt-stat">
-                  {{ statLabels[stat.name] || stat.name }}:
-                  <span>
-                    {{
-                      ["crit_rate", "hit_rate", "lifesteal"].includes(stat.name)
-                        ? stat.value + "%"
-                        : stat.value
-                    }}
-                  </span>
+    <div class="top-sectionMarket">
+      <div class="left-panelMarket">
+        <h2 class="tt-stats">Inventory</h2>
+        <div class="inventory-grid">
+          <div v-for="item in gridInventory" class="inventory-item">
+            <template v-if="item">
+              <div class="tooltip-container">
+                <img :src="generateImageName(item.item.name, item.item.rarity)" :alt="item.name" class="item-icon"
+                  @error="handleImageError" />
+                <div class="custom-tooltip">
+                  <div class="tt-font-name" :class="`rarity-${item.item.rarity}`">
+                    {{ item.item.name }}
+                  </div>
+                  <div class="tt-font">
+                    Required Level: {{ item.item.required_level }}
+                  </div>
+                  <div class="tt-stats" v-if="item.item.stats && item.item.stats.length">
+                    Stats:
+                    <div v-for="(stat, sidx) in item.item.stats" :key="sidx" class="tt-stat">
+                      {{ statLabels[stat.name] || stat.name }}:
+                      <span>
+                        {{
+                          ["crit_rate", "hit_rate", "lifesteal"].includes(stat.name)
+                            ? stat.value + "%"
+                            : stat.value
+                        }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <button class="inventory-action-btn" @click="activeItemId = item.id">Sell</button>
+                <div v-if="activeItemId === item.id" style="margin-top: 6px;">
+                  <input type="number" v-model.number="prices[item.id]" placeholder="Enter price"
+                    style="width: 80px; padding: 4px; font-size: 0.8rem; border-radius: 0.3rem;" />
+                  <button @click="confirmSell(item.id)"
+                    style="margin-left: 5px; padding: 4px 8px; font-size: 0.8rem; border-radius: 0.3rem;">
+                    Confirm
+                  </button>
                 </div>
               </div>
-            </div>
-            <button class="inventory-action-btn" @click="activeItemId = item.id">Sell</button>
-            <div v-if="activeItemId === item.id" style="margin-top: 6px;">
-              <input type="number" v-model.number="prices[item.id]" placeholder="Enter price"
-                style="width: 80px; padding: 4px; font-size: 0.8rem; border-radius: 0.3rem;" />
-              <button @click="confirmSell(item.id)"
-                style="margin-left: 5px; padding: 4px 8px; font-size: 0.8rem; border-radius: 0.3rem;">
-                Confirm
-              </button>
-            </div>
-           
-          </div> 
-        </template>
-        <template v-else>
-          <div class="item-icon" style="opacity: 0.2">Empty</div>
-        </template>
+            </template>
+            <template v-else>
+              <div class="item-icon" style="opacity: 0.2">Empty</div>
+            </template>
+          </div>
+        </div>
+      </div>
+      <div class="right-panel">
+        <h2 class="tt-stats">Listed Items</h2>
+        <div class="inventory-grid">
+          <div v-for="item in listed_items" class="inventory-item">
+            <template v-if="item">
+              <div class="tooltip-container">
+                <img :src="generateImageName(item.item.item.name, item.item.item.rarity)" :alt="item.name"
+                  class="item-icon" @error="handleImageError" />
+                <div class="custom-tooltip">
+                  <div class="tt-font-name" :class="`rarity-${item.item.item.rarity}`">
+                    {{ item.item.item.name }}
+                  </div>
+                  <div class="tt-font">
+                    Required Level: {{ item.item.item.required_level }}
+                  </div>
+                  <div class="tt-stats" v-if="item.item.item.stats && item.item.item.stats.length">
+                    Stats:
+                    <div v-for="(stat, sidx) in item.item.item.stats" :key="sidx" class="tt-stat">
+                      {{ statLabels[stat.name] || stat.name }}:
+                      <span>
+                        {{
+                          ["crit_rate", "hit_rate", "lifesteal"].includes(stat.name)
+                            ? stat.value + "%"
+                            : stat.value
+                        }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="tt-font">
+                  Price: {{ item.price }}🟡
+                </div>
+                <button class="inventory-action-btn" @click="CancelSell(item.id)">Cancel</button>
+              </div>
+            </template>
+            <template v-else>
+              <div class="item-icon" style="opacity: 0.2">Empty</div>
+            </template>
+          </div>
+        </div>
       </div>
     </div>
-    <div class ="stat-line">Gold:{{ profile.gold }} 🟡</div>
-  </div>
-  <div>
-    <!-- <div v-for="item in marketplace" :key="item.id">
-      {{ item.price }} gold - {{ item.item.item.name }} - seller {{ item.seller }}
-    </div> -->
+    <h2 class="tt-stats">Marketplace</h2>
     <div class="inventory-grid">
       <div v-for="item in marketplace" class="inventory-item">
         <template v-if="item">
@@ -234,12 +317,12 @@ async function BuyItem(itemId) {
                   </span>
                 </div>
               </div>
-              <div class="tt-font">
-                Seller: {{ item.seller }}
-              </div>
-              <div class="tt-font">
-                Price: {{ item.price }}🟡
-              </div>
+            </div>
+            <div class="tt-font">
+              Seller: {{ item.seller }}
+            </div>
+            <div class="tt-font">
+              Price: {{ item.price }}🟡
             </div>
             <button class="inventory-action-btn" @click="BuyItem(item.id)">Buy</button>
           </div>
@@ -249,168 +332,27 @@ async function BuyItem(itemId) {
         </template>
       </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
-.main {
-  width: 95vw;
-  max-width: 1200px;
-  min-width: 320px;
-  min-height: 40vh;
-  margin: 5vw;
-  background: #000;
-  position: relative;
-  box-sizing: border-box;
-  border-radius: 1.2rem;
+.top-sectionMarket {
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: stretch;
-}
-
-.inventory-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(90px, 1fr));
-  gap: 0.7rem;
-  margin-top: 1vw;
+  flex-wrap: wrap;
+  gap: 0.4vw;
+  justify-content: space-between;
   width: 100%;
+  margin-bottom: 1vw;
+}
+
+.left-panelMarket,
+.right-panelMarket {
+  flex: 1 1 350px;
+  min-width: 280px;
   box-sizing: border-box;
-}
-
-.inventory-item {
-  background: #222;
-  border-radius: 0.75rem;
-  padding: 0.75rem;
-  text-align: center;
-  color: #eee;
-  cursor: pointer;
-  transition: transform 0.2s;
-  min-width: 70px;
-  min-height: 70px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.tooltip-container {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.item-icon {
-  width: 5vw;
-  height: 4vw;
-  max-width: 90px;
-  max-height: 90px;
-  min-width: 50px;
-  min-height: 50px;
-  margin-bottom: 0.5rem;
-}
-
-.inventory-action-btn {
-  display: none;
-  background: #444;
-  color: #fff;
-  border: none;
-  border-radius: 0.5rem;
-  padding: 0.25rem 0.5rem;
-  cursor: pointer;
-  font-size: 0.85rem;
-  transition: background-color 0.2s, transform 0.2s;
-}
-
-.custom-tooltip {
-  display: none;
-  position: absolute;
-  z-index: 1000;
-  left: 50%;
-  top: -10px;
-  transform: translate(-50%, -100%);
-  /* transform: translateX(0, -50%); */
-  background: #222;
-  color: #fff;
-  border-radius: 0.5rem;
-  padding: 0.75rem 1rem;
-  min-width: 180px;
-  box-shadow: 0 0 10px #000a;
-  font-size: 0.95rem;
-  pointer-events: none;
-  white-space: pre-line;
-}
-
-.tt-font-name {
-  font-weight: bold;
-  font-size: 1.05rem;
-  margin-bottom: 0.25rem;
-}
-
-.tt-font {
-  font-weight: bold;
-  font-size: 0.9rem;
-  margin-bottom: 0.25rem;
-}
-
-.tt-stats {
-  margin-top: 0.5rem;
-}
-
-.tt-stat {
-  font-size: 0.85rem;
-  color: #b3e5fc;
-}
-
-.tt-value {
-  color: gold;
-  font-size: 0.9rem;
-  margin-bottom: 0.25rem;
-  font-weight: bold;
-}
-
-.tt-rarity {
-  color: #ccc;
-  margin-bottom: 0.25rem;
-}
-
-.rarity-common {
-  color: #fff;
-}
-
-.rarity-uncommon {
-  color: #4caf50;
-}
-
-.rarity-rare {
-  color: #2196f3;
-}
-
-.rarity-epic {
-  color: #9c27b0;
-}
-
-.rarity-legendary {
-  color: #ff9800;
-}
-
-.inventory-item:hover .inventory-action-btn {
-  display: block;
-}
-
-.tooltip-container:hover .custom-tooltip {
-  display: block;
-}
-
-.tooltip-container:hover .custom-tooltip-equipped {
-  display: block;
-}
-
-.stat-line {
-  font-size: 1.5rem;
-  padding: 0.25rem 0;
-  color: white;
+  border: 1px solid yellow;
+  padding: 1vw;
+  background: transparent;
+  border-radius: 0.7rem;
 }
 </style>
